268f76e597f4eec06b19f97bcf990beca2c00081
Son Tung <tungtshp99@gmail.com>
2024-12-23 09:37:13 +0700

    hide change app icon dialog

--- ios/Classes/VariableAppIconPlugin.swift
+++ ios/Classes/VariableAppIconPlugin.swift
@@ -16,10 +16,24 @@ public class VariableAppIconPlugin: NSObject, FlutterPlugin {
     case "changeAppIcon":
       let defaultIcon = arguments["defaultiOS"] as! String
       let currentIcon = arguments["iosIcon"] as! String
-      UIApplication.shared.setAlternateIconName(defaultIcon == currentIcon ? nil : currentIcon)
+        setApplicationIconName(defaultIcon == currentIcon ? nil : currentIcon)
 
     default:
       result(FlutterMethodNotImplemented)
     }
   }
+    
+    func setApplicationIconName(_ iconName: String?) {
+            if UIApplication.shared.responds(to: #selector(getter: UIApplication.supportsAlternateIcons)) && UIApplication.shared.supportsAlternateIcons {
+                
+                typealias setAlternateIconName = @convention(c) (NSObject, Selector, NSString?, @escaping (NSError) -> ()) -> ()
+                
+                let selectorString = "_setAlternateIconName:completionHandler:"
+                
+                let selector = NSSelectorFromString(selectorString)
+                let imp = UIApplication.shared.method(for: selector)
+                let method = unsafeBitCast(imp, to: setAlternateIconName.self)
+                method(UIApplication.shared, selector, iconName as NSString?, { _ in })
+            }
+        }
 }
